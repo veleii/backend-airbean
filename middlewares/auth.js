80c3995fdb3546dc587c9904958ffc05080c7b75
Lilla Mry <maria.kjellholm@gmail.com>
2025-04-14 20:22:00 +0200

    skapat auth för användare(token)

---
+++ middlewares/auth.js
@@ -0,0 +1,25 @@
+import { access } from "fs";
+import jwt from "jsonwebtoken";
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ error: "Åtkomst nekad, token saknas!" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ error: "Åtkomst nekad, token saknas!" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || "yourSecretKey")
+    req.user = decoded
+    next()
+  } catch (error) {
+    console.error("Error vid token-verifiering: ", error);
+    return res.status(401).json({ error: "Ogiltig token."})
+  }
+};
+
+export default authMiddleware;
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { addOrder, getOrderById } from "../controllers/orderController.js"
+import { addOrder, getOrderById } from "../controllers/orderController.js";
+import authMiddleware from "../middlewares/auth.js";
+
 const router = Router();
 
-router.post("/order", addOrder)
+router.post("/order", authMiddleware, addOrder);
 
-
-router.get("/:id", getOrderById)
-
+router.get("/:id", authMiddleware, getOrderById);
 
 export default router;
